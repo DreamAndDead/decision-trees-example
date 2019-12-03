7fdb0b99c8e02b2b149462b83dd42e487bd8ea55
DreamAndDead <favorofife@yeah.net>
2019-12-03 18:51:48 +0800

    choose feature base on entropy

--- decision_trees.py
+++ decision_trees.py
@@ -1,69 +1,121 @@
 #!/usr/bin/env python3
 
-"""
-dataset: https://archive.ics.uci.edu/ml/datasets/Adult
-"""
-
 from graphviz import Digraph
-import pandas as pd
-
-ad = pd.read_csv('dataset/adult.data', sep=', ', header=None, names=['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country', 'target'], na_values='?', keep_default_na=False)
-
-
-cat = ad.drop(columns=['age', 'fnlwgt', 'education_num', 'capital_gain', 'capital_loss', 'hours_per_week']).dropna()
-
-
-g = Digraph('g', filename='tree.gv')
-
+import math
 
 class Node:
-    def __init__(self, attr):
-        self.attr = attr
+    count = 0
+    graph = Digraph('tree', filename='tree.gv')
+    
+    def __init__(self, feature):
+        self.number = str(Node.count)
+        Node.count += 1
+        
+        self.feature = feature
         self.children = {}
+        Node.graph.node(self.number, feature)
 
-    def add_child(self, attr, child):
-        self.children[attr] = child
+    def add_child(self, value, child):
+        self.children[value] = child
+        Node.graph.edge(self.number, child.number, label=value)
 
     def is_leaf(self):
         return len(self.children) == 0
 
-attr_values = {}
-for k in cat.keys():
-    c = cat[k]
-    attr_values[k] = c.unique()
+    def reset_graph(self):
+        Node.graph.clear()
 
+    def view_graph(self):
+        Node.graph.view()
+
+def entropy(data):
+    """
+    equals sum -p*log2(p)
+    """
+    target = data['target']
+    count = target.value_counts()
+    sum = len(target)
+
+    entro = 0
+    for k in count.keys():
+        p = count[k] / sum
+        if p == 0:
+            entro += 0
+        else:
+            entro += (-p * math.log2(p))
+
+    return entro
+
+def pick_feature_by_entropy(data):
+    """return feature name"""
+    origin_entro = entropy(data)
+
+    entro_gain = 0
+    feature = None
+    
+    features = data.columns.drop('target')
+    for f in features:
+        col = data[f]
+        sum = len(col)
+
+        count = col.value_counts()
+        new_entro = 0
+        for k in count.keys():
+            p = count[k] / sum
+            new_entro += p * entropy(data[col==k])
+
+        gain = origin_entro - new_entro
+        if gain > entro_gain:
+            entro_gain = gain
+            feature = f
+
+    return feature
     
 def decision_tree(data):
-    target = data['target']
-    most_target = target.value_counts().keys()[0]
-    
-    # if all target is same
-    if target.nunique() == 1:
-        leaf = Leaf(target.unique()[0])
-        return leaf
+    feature_values = {}
+    for k in data.keys():
+        feature_values[k] = data[k].unique()
 
-    # if attr is empty, return the most target
-    if data.shape[1] == 1:
-        leaf = Leaf(most_target)
-        return leaf
+    def _decision_tree(data):
+        target = data['target']
+        most_target = target.value_counts().keys()[0]
+        
+        # if all target is same
+        if target.nunique() == 1:
+            node = Node(target.unique()[0])
+            return node
 
-    # pick attr
-    attr_name = data.keys()[0]
-    attr = data[attr_name]
+        # if feature is empty, return the most target
+        if data.shape[1] == 1:
+            node = Node(most_target)
+            return node
 
-    # split branch
-    branch = Branch(attr_name)
-    for v in attr_values[attr_name]:
-        child_data = data[attr==v].drop(columns=[attr_name])
-        if child_data.shape[0] == 0:
-            leaf = Leaf(most_target)
-            return leaf
-        else:
-            child = decision_tree(child_data)
-            branch.add_child(v, child)
+        # pick feature
+        feature_name = pick_feature_by_entropy(data)
+        feature = data[feature_name]
 
-    return branch
+        # split node
+        node = Node(feature_name)
+        for v in feature_values[feature_name]:
+            child_data = data[feature==v].drop(columns=[feature_name])
+            if child_data.shape[0] == 0:
+                child = Node(most_target)
+                node.add_child(v, child)
+            else:
+                child = _decision_tree(child_data)
+                node.add_child(v, child)
+
+        return node
+
+    tree = _decision_tree(data)
+    return tree
 
 
+def predict(tree, data):
+    if tree.is_leaf():
+        return tree.feature
 
-t = decision_tree(cat)
+    feature = tree.feature
+    feature_value = data[feature]
+    return predict(tree.children[feature_value], data)
+
--- test_decision_trees.py
+++ test_decision_trees.py
@@ -1,9 +1,28 @@
+"""
+dataset: https://archive.ics.uci.edu/ml/datasets/Adult
+"""
+
 import unittest
+import pandas as pd
+import decision_trees as dt
 
 class TestDecisionTrees(unittest.TestCase):
     def setUp(self):
-        pass
+        self.data = pd.read_csv('dataset/adult.data', sep=', ', na_values='?', keep_default_na=False, engine='python')
+
+        self.cat_data = self.data.drop(columns=['age', 'fnlwgt', 'education_num', 'capital_gain', 'capital_loss', 'hours_per_week']).dropna()
+
+        self.data = self.cat_data.iloc[:100]
+
+        self.tree = dt.decision_tree(self.data)
+
+    def tearDown(self):
+        self.tree.reset_graph()
     
-    def test_t(self):
-        self.assertEqual(0, 0)
+    def test_origin(self):
+        for _, row in self.data.iterrows():
+            res = dt.predict(self.tree, row)
+            self.assertEqual(res, row['target'])
+
+        self.tree.view_graph()
 
