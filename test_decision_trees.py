bff8d3a8a52b478828891617013ba609412afb4f
DreamAndDead <favorofife@yeah.net>
2019-12-13 19:02:56 +0800

    split data into test and train

--- test_decision_trees.py
+++ test_decision_trees.py
@@ -4,14 +4,15 @@ import decision_trees as dt
 
 class TestDecisionTrees(unittest.TestCase):
     def setUp(self):
-        self.data = pd.read_excel('triple_all.xlsx')
-        self.tree = dt.decision_tree(self.data)
+        self.train = pd.read_excel('dataset/offence_train.xlsx')
+        self.test = pd.read_excel('dataset/offence_test.xlsx')
+        self.tree = dt.decision_tree(self.train)
 
     def tearDown(self):
         self.tree.reset_graph()
     
     def test_origin(self):
-        for _, row in self.data.iterrows():
+        for _, row in self.test.iterrows():
             res = dt.predict(self.tree, row)
             self.assertEqual(res, row['target'])
 
